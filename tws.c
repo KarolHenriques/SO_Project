c6edf25af73b17680f3dc534b4be756a47a8fbb8
Karol Henriques <karoljares@gmail.com>
2023-04-21 03:33:08 +0100

    Pool of processes. Still with some problems

--- tws.c
+++ tws.c
@@ -21,6 +21,10 @@
 #include <sys/time.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <semaphore.h> // for semaphore functions
+#include <sys/wait.h>  // for waitpid() function
+#include <stdbool.h>
+
 
 #define BUFSIZE         8096
 #define ERROR           42
@@ -29,10 +33,11 @@
 #define NOTFOUND        404
 #define VERSION         1
 #define MAX_POOL_SIZE   10
-#define SHM_SIZE        1024
+#define SHM_KEY         1234
+#define SHM_SIZE        10
+#define INFO            2
 
 /* Shared memory to pass the request to a new child if the one who's handling the request ends abruptly*/
-
 int shmid;
 void *shmaddr;
 
@@ -76,21 +81,6 @@ void logger(int type, char *s1, char *s2, int socket_fd){
         (void)write(fd,logbuffer,strlen(logbuffer));
         (void)write(fd,"\n",1);
         (void)close(fd);
-    }
-}
-
-/* handle signal for SIGCHLD*/
-void handle_sigchld(int sig) {
-    pid_t pid;
-    int *status = NULL;
-    while ((pid = waitpid(-1, status, WNOHANG)) > 0) {
-        // Child process has exited, write PID to shared memory segment
-        *(pid_t*)shmaddr = pid;
-    }
-    
-    if (pid == -1 && errno != ECHILD) {
-        // Error occurred
-        printf("Error in waitpid(): %s\n", strerror(errno));
     }
 }
 
@@ -152,8 +142,8 @@ int web(int fd, int hit){
         return 1;
     }
     
-    if(( file_fd = open(&buffer[5],O_RDONLY)) == -1) {  /* open the file for reading */
-        logger(NOTFOUND, "failed to open file",&buffer[5],fd);
+    if((file_fd = open(&buffer[5],O_RDONLY)) == -1) {  /* open the file for reading */
+        logger(NOTFOUND, "failed to open file", &buffer[5], fd);
         close(fd);
         return 1;
     }
@@ -165,7 +155,7 @@ int web(int fd, int hit){
     (void)write(fd,buffer,strlen(buffer));
     
     /* send file in 8KB block - last block may be smaller */
-    while (    (ret = read(file_fd, buffer, BUFSIZE)) > 0 ) {
+    while ((ret = read(file_fd, buffer, BUFSIZE)) > 0 ) {
         (void)write(fd,buffer,ret);
     }
     sleep(1);    /* allow socket to drain before signalling the socket is closed */
@@ -181,29 +171,25 @@ int main(int argc, char **argv, char** envp){
     static struct sockaddr_in cli_addr; /* static = initialised to zeros */
     static struct sockaddr_in serv_addr; /* static = initialised to zeros */
     
-    //Pool variables
-    int pool_size = 0;
-    int status;
-    
     //Shared memory variables
-    shmid = shmget(IPC_PRIVATE, SHM_SIZE, IPC_CREAT | 0666);
-    if (shmid == -1) {
-        perror("shmget");
-        exit(1);
-    }
-    
-    // Attach shared memory segment to process
-    shmaddr = shmat(shmid, NULL, 0);
-    if (shmaddr == (void*)-1) {
-        perror("Failed to attach shared memory segment");
-        exit(1);
-    }
-    
-    // Set up signal handler for SIGCHLD
-    if (signal(SIGCHLD, handle_sigchld) == SIG_ERR) {
-        perror("Failed to set up signal handler for SIGCHLD");
-        exit(1);
-    }
+    /*shmid = shmget(IPC_PRIVATE, SHM_SIZE, IPC_CREAT | 0666);
+     if (shmid == -1) {
+     perror("shmget");
+     exit(1);
+     }
+     
+     // Attach shared memory segment to process
+     shmaddr = shmat(shmid, NULL, 0);
+     if (shmaddr == (void*)-1) {
+     perror("Failed to attach shared memory segment");
+     exit(1);
+     }
+     
+     // Set up signal handler for SIGCHLD
+     if (signal(SIGCHLD, handle_sigchld) == SIG_ERR) {
+     perror("Failed to set up signal handler for SIGCHLD");
+     exit(1);
+     }*/
     
     if( argc < 3  || argc > 3 || !strcmp(argv[1], "-?") ) {
         (void)printf("\n\nhint: ./tws Port-Number Top-Directory\t\tversion %d\n\n"
@@ -235,7 +221,7 @@ int main(int argc, char **argv, char** envp){
     logger(LOG,"tws starting",argv[1],getpid());
     
     /* setup the network socket */
-    /*if((listenfd = socket(AF_INET, SOCK_STREAM,0)) <0)
+    if((listenfd = socket(AF_INET, SOCK_STREAM,0)) <0)
         logger(ERROR, "system call","socket",0);
     port = atoi(argv[1]);
     if(port < 0 || port >60000)
@@ -245,97 +231,202 @@ int main(int argc, char **argv, char** envp){
     serv_addr.sin_port = htons(port);
     if(bind(listenfd, (struct sockaddr *)&serv_addr,sizeof(serv_addr)) <0)
         logger(ERROR,"system call","bind",0);
-    if( listen(listenfd,64) <0)
-        logger(ERROR,"system call","listen",0);
+    /********************************If we are not using a pool**I*****************************************************************/
+    /*if(listen(listenfd,64) <0)
+     logger(ERROR,"system call","listen",0);*/
     
-    for(hit=1; ;hit++) {
-        length = sizeof(cli_addr);
-        //Block waiting for clients
-        socketfd = accept(listenfd, (struct sockaddr *)&cli_addr, &length);
-        if (socketfd<0)
-            logger(ERROR,"system call","accept",0);
-        else
-            web(socketfd,hit);
-    }
-    (void)close(listenfd);*/
+    /*for(hit=1; ;hit++) {
+     length = sizeof(cli_addr);
+     //Block waiting for clients
+     socketfd = accept(listenfd, (struct sockaddr *)&cli_addr, &length);
+     if (socketfd<0)
+     logger(ERROR,"system call","accept",0);
+     else
+     web(socketfd,hit);
+     }
+     (void)close(listenfd);*/
     
     /********************************Child to handle each request*********************************/
-    /* while(1){
-        socketfd = accept(listenfd, (struct sockaddr *)&cli_addr, &length);
-        if(socketfd < 0){
-            logger(ERROR,"system call","accept",0);
-            continue;
-        }
-        // fork new child process to handle client request
-        pid = fork();
-        if (pid < 0) {
-            logger(ERROR,"fork failed","fork",0);
-            close(socketfd);
-            continue;
-        }else if(pid == 0){//child process to handle the client's request
-            web(socketfd,hit);
-            exit(EXIT_SUCCESS);
-            
-        }
-        close(socketfd);
-    }*/
-    /********************************Pool of process***************************************/
-    /*printf("I'm here!\n");
-     pid_t pidArray[MAX_POOL_SIZE];
-     int num_children = 0;
-     
-     for (int i = 0; i < MAX_POOL_SIZE; i++) {
-     pidArray[i] = fork();
-     if (pidArray[i] < 0) {
-     logger(ERROR,"fork failed","fork",0);
-     }else if (pidArray[i] == 0) {//child process
-     while (1) {
+    /*while(1){
      socketfd = accept(listenfd, (struct sockaddr *)&cli_addr, &length);
      if(socketfd < 0){
-     close(socketfd);
      logger(ERROR,"system call","accept",0);
      continue;
      }
-     web(socketfd,hit);
-     close(socketfd);
-     }
-     exit(EXIT_SUCCESS);
-     }
-     }
-     // parent process
-     while(1){
-     wait(NULL); //wait for any child to finish
-     num_children--;
-     printf("We got here because it was needed\n");
-     
-     // fork new child process if necessary
-     if (num_children < MAX_POOL_SIZE){
-     pidArray[num_children] = fork();
-     if (pidArray[num_children] < 0) {
+     // fork new child process to handle client request
+     pid = fork();
+     if (pid < 0) {
      logger(ERROR,"fork failed","fork",0);
      close(socketfd);
      continue;
      }
-     else if (pidArray[num_children] == 0) {//child process
-     while (1) {
-     //for(int i = 0;  i < num_children; i++){
-     socketfd = accept(listenfd, (struct sockaddr *)&cli_addr, &length);
-     //}
-     if(socketfd < 0){
-     close(socketfd);
-     logger(ERROR,"system call", "accept", 0);
-     continue;
-     }
+     if(pid == 0){//child process to handle the client's request
      web(socketfd,hit);
+     exit(EXIT_SUCCESS);
+     }
+     if(pid > 0){ //PARENT PROCESS ADDED WITHOUT TEST
+     wait(NULL); // wait for child process to finish before creating another one
+     }
      close(socketfd);
-     }
-     
-     }
-     num_children++;
-     }
-     return 0;
      }*/
+    /********************************Pool of process***************************************/
+    int pidArray[MAX_POOL_SIZE];
+    int num_children = 0;
+    pid_t pid_Pool;
+    int shm_id, *shm_ptr;
     
-    // }
+    // Create shared memory for process availability flags
+    shm_id = shmget(SHM_KEY, SHM_SIZE * sizeof(int), IPC_CREAT | 0666);
+    
+    if (shm_id < 0) {
+        logger(ERROR, "shmget failed", "shmget", 0);
+        exit(1);
+    }
+    shm_ptr = (int*)shmat(shm_id, NULL, 0);
+    if (shm_ptr == (int*)-1) {
+        logger(ERROR, "shmat failed", "shmat", 0);
+        exit(1);
+    }
+    
+    sem_t *sem;
+    sem = sem_open("semaphore", O_CREAT, 0644, 1);
+    if (sem == SEM_FAILED) {
+        logger(ERROR, "sem_open failed", "sem_open", 0);
+        exit(1);
+    }
+    
+    for (int i = 0; i < MAX_POOL_SIZE; i++) {
+        shm_ptr[i] = 1; // Initialize all flags to 1 (available)
+    }
+    // Create child processes
+    for (int i = 0; i < MAX_POOL_SIZE; i++) {
+        pid_Pool = fork();
+        if (pid_Pool < 0) {
+            logger(ERROR,"fork failed","fork",0);
+        }
+        else if(pid_Pool == 0){
+            // child process
+            //break;
+            bool request_available;
+            while(1){
+                //Wait for a request to be assigned by the parent process
+                printf("I'm here!");
+                sem_wait(sem);
+                
+                if (getppid() == 1) { //check if the parent exited
+                    break;
+                }
+                
+                // Read the socket file descriptor from shared memory
+                int* socketfd_ptr = (int*)(shm_ptr + SHM_SIZE);
+                int socketfd = socketfd_ptr[i];
+                
+                if (socketfd > 0) {
+                    printf("My index value is: %d\n", i);
+                    
+                    //Handle the request
+                    web(socketfd, hit);
+                    
+                    printf("Done, dad\n");
+                    
+                    shm_ptr[i] = 1; //Set availability flag to 1 (available)
+                    
+                    close(socketfd);
+                    //Notify the parent process that we're done handling the request
+                    sem_post(sem);
+                    request_available = false;
+                }
+                else if (!request_available) {
+                    printf("No requests available, sleeping...\n");
+                    request_available = true;
+                    sleep(1); // Sleep for 1 second before checking again
+                }
+            }
+        }
+        else{
+            pidArray[i] = pid_Pool;
+            num_children++;
+        }
+    }
+    
+    //Parent process
+    while(1){
+        //parent process accepts the request and assign to a child
+        if(listen(listenfd, 64) < 0){
+            logger(ERROR,"system call","listen",0);
+            continue;
+        }
+        /*int*/ socketfd = accept(listenfd, (struct sockaddr *)&cli_addr, &length);
+        if (socketfd < 0) {
+            close(socketfd);
+            logger(ERROR,"system call","accept",0);
+            continue;
+        }
+        // Find next available child process
+        int index = -1;
+        for (int i = 0; i < SHM_SIZE; i++) {
+            if (shm_ptr[i] == 1) {
+                index = i;
+                shm_ptr[i] = 0; // Set availability flag to 0 (busy)
+                printf("Selecting child %d to handle the request \n", pidArray[index]);
+                close(socketfd);
+                
+                //send the socket to the shared memory
+                int* socketfd_ptr = (int*)(shm_ptr + SHM_SIZE);
+                socketfd_ptr[i] = socketfd;
+                
+                sem_post(sem);
+                break;
+            }
+        }
+        if (index == -1) {
+            logger(ERROR, "no available child processes", "main", 0);
+            continue;
+        }
+        
+        /*else {
+         sleep(1); // Wait for 1 second before checking for requests again
+         }*/
+    }
+    
+    // Detach shared memory
+    if (shmdt(shm_ptr) == -1) {
+        logger(ERROR, "shmdt failed", "shmdt", 0);
+        exit(1);
+    }
+    
+    // Remove shared memory
+    if (shmctl(shm_id, IPC_RMID, 0) == -1) {
+        logger(ERROR, "shmctl(IPC_RMID) failed", "shmctl", 0);
+        exit(1);
+    }
+    
+    // Close listening socket
+    close(listenfd);
+    
+    // Kill child processes
+    for (int i = 0; i < num_children; i++) {
+        kill(pidArray[i], SIGTERM);
+    }
+    
+    // Wait for all child processes to exit
+    while (num_children > 0) {
+        pid_t pid = wait(NULL);
+        for (int i = 0; i < num_children; i++) {
+            if (pidArray[i] == pid) {
+                logger(INFO, "Child process exited", "", pid);
+                // Remove child process from array and shift remaining processes left
+                for (int j = i; j < num_children - 1; j++) {
+                    pidArray[j] = pidArray[j + 1];
+                }
+                num_children--;
+                break;
+            }
+        }
+    }
 }
+
+
+
+
 
